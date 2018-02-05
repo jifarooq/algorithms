3acefc2a2346d7e235d557d5b43bfbb5c89a7f1d
Justin Farooq <justinfarooq@gmail.com>
2018-02-04 22:43:28 -0800

    fix spacing

--- living_people.py
+++ living_people.py
@@ -11,27 +11,27 @@ For example, Person (birth= 1908, death= 1909) is included in the counts for bot
 
 # runs in O(P log P) time with P = number of people or year-pairs
 def most_living_people(year_pairs):
-	births = set(map(lambda x:x[0], year_pairs))
-	deaths = set(map(lambda x:x[1], year_pairs))
-	sorted_yrs = sorted(births.union(deaths))
+  births = set(map(lambda x:x[0], year_pairs))
+  deaths = set(map(lambda x:x[1], year_pairs))
+  sorted_yrs = sorted(births.union(deaths))
 
-	pops = {}
-	pops[sorted_yrs[0]] = 1
+  pops = {}
+  pops[sorted_yrs[0]] = 1
 
-	for i, yr in enumerate(sorted_yrs):
-		if i == 0:
-			continue
-		delta = 1 if (yr in births) else -1
-		last = sorted_yrs[i - 1]
-		pops[yr] = pops[last] + delta
+  for i, yr in enumerate(sorted_yrs):
+    if i == 0:
+      continue
+    delta = 1 if (yr in births) else -1
+    last = sorted_yrs[i - 1]
+    pops[yr] = pops[last] + delta
 
-	return max(pops, key=pops.get)
+  return max(pops, key=pops.get)
 
 
 class TestLivingPeople(unittest.TestCase):
-	def test_normal_use_case(self):
-		persons = [(1920, 1939), (1911, 1944), (1920, 1955), (1938, 1939)]
-		self.assertEqual(most_living_people(persons), 1938)
+  def test_normal_use_case(self):
+    persons = [(1920, 1939), (1911, 1944), (1920, 1955), (1938, 1939)]
+    self.assertEqual(most_living_people(persons), 1938)
 
 
 if __name__ == '__main__':
